79ec1d5d73f64c6349d9c2f80b42a464ebaf9d18
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2023-10-07 00:40:31 +0530

    11

---
+++ question11.py.py
@@ -0,0 +1,9 @@
+person={
+  "name":"Manish Bhardwaj",
+  "age" :18,
+  "studying":"Btech CSE AIML & IOT"
+  }
+
+info = f"{person['name']} is {person['age']} years of old, studying in {person['studying']}"
+
+print(info)
